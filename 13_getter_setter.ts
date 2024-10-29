6cdbdb452680a42ac4e675fb3c74b76309c85c3f
dev <thakurdev2672@gmail.com>
2024-10-29 23:45:46 +0530

    getterand setter

--- 13_getter_setter.ts
+++ 13_getter_setter.ts
@@ -1,1 +1,32 @@
 /* in this we will learn about getter and setter in typescript */
+
+class User {
+    
+    private _courseCount=1;
+
+    readonly city:string="jaipur"
+
+    constructor(private name:string, public email:string ,private userId:string) {
+    this.email=email;
+    this.name=name;
+    // this.userId=userId;
+        
+    }
+
+ 
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if(courseNum<=1){
+         throw new Error(`Course count should be more than 1`)
+        }
+        this._courseCount=courseNum
+    }
+
+}
