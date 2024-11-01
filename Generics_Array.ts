c2242e2334a0410c66d18a225c76d0ca509b0397
dev <thakurdev2672@gmail.com>
2024-11-01 17:46:22 +0530

    generics array func

---
+++ Generics_Array.ts
@@ -0,0 +1,16 @@
+function getSearchProducts <T>(products:T[]):T{
+    //do some database opern
+    const myIndex=3;
+    return products[myIndex];
+}
+
+  
+//  <T,>  is uses to differentiate among tags li h1
+
+//  arraw function...
+
+// function getMoreSearchProducts = <T,>(products:T[]):T => {
+//     //do some database opern
+//     const myIndex=4;
+//     return products[myIndex];
+// }
